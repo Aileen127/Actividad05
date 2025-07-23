6ea3726bb99508e3a324a054da27a557ff2f915a
Aileen127 <fleuraileen394@gmail.com>
2025-07-22 20:00:51 -0600

    Mejora de caso 5, mejora de print, mejora en caso 6 de mal print eliminación de guiones

--- actividad_05.py
+++ actividad_05.py
@@ -8,7 +8,7 @@ while True:
     print("3. Calcular la venta más alta y la más baja")
     print("4. Calcular promedio de ventas")
     print("5. Contar cuántos días superaron los Q1000")
-    print("6. Clasificar cada venta: alta (>1000), memdia (500-1000), baja (<500)")
+    print("6. Clasificación de ventas (alta, media, baja")
     print("7. Salir")
 
     option = input("Ingresa una opción del menú por favor: ")
@@ -45,20 +45,32 @@ while True:
             for i in ventas:
                 suma = sum(ventas)
                 promedio = sum(ventas) / len(ventas)
-            print(promedio)
+
+            print(f"Promedio de ventas: {promedio}")
 
 
         # Cuantos días superaron los 1000
         case "5":
             if ventas:
+                dias = 0
                 for i in ventas:
-                    dias = 0
+
                     if i > 1000:
                         dias += 1
-                        print(f"La cantidad de días que superaron los Q1000 son {i}")
+                print(f"La cantidad de días que superaron los Q1000 son {dias}")
+            else:
+                    print("No hay ventas ingresadas")
+        # Clasificación de cada venta
+        case "6":
+            for venta in ventas:
+                if venta > 1000:
+                    print(f"Venta alta \n  {venta}")
+                elif venta >= 500 and venta <= 1000:
+                    print(f"Venta media \n  {venta}")
+                elif venta < 500:
+                    print(f"Venta baja \n  {venta}")
                 else:
-                    print("No hay días que superen los Q1000")
-
+                    print("No hay ventas registradas.")
         case "7":
-            print("Ha salido")
+            print("Ha salido del menú de ventas, que tenga un buen día")
             break
